ac5079925d5a7d29c9d18fb8e9afd00fed8f0722
germanheinz <heinz.german@gmail.com>
2020-05-20 17:29:55 +0200

    Chat Page - Saving Data

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      initialRoute: LoginPage.id,
+      initialRoute: ChatPage.id,
       routes: {
         HomePage.id     : (BuildContext context) => HomePage(),
         RegisterPage.id : (BuildContext context) => RegisterPage(),
--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class ChatPage extends StatefulWidget {
   static const String id = 'chat';
@@ -9,9 +11,10 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
-  
+  final _fireStore = Firestore.instance;
   final _auth = FirebaseAuth.instance;
   FirebaseUser firebaseUser;
+  String message;
 
   @override
   void initState() { 
@@ -33,9 +36,45 @@ class _ChatPageState extends State<ChatPage> {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chat'),
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          
+          children: <Widget>[
+            _inputMessage(),
+          ],
+        ),
+      ),
+      // floatingActionButton: Icon(Icons.send),
+    );
+  }
+  Widget _inputMessage(){
     return Container(
-      child: Text(
-        'Chat Page'
+      margin: const EdgeInsets.only(left: 10.0, right: 0.0,bottom: 5.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+              hintText: 'Write a Message',
+              border: OutlineInputBorder(),
+              ),
+            onChanged:(value){
+              message = value;
+            } 
+            ),
+          ),
+          FlatButton(
+              onPressed: (){
+                _fireStore.collection('messages').add({'message': message ,'sender': firebaseUser.email});
+              },
+              child: Text('Send'),
+          ),
+        ],
       ),
     );
   }
